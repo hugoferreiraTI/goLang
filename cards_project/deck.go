20257e84fb59ed709df81e8afe1f20187825fece
Hugo Ferreira Ferro <hugoferreiraferro1@hotmail.com>
2026-09-16 14:35:34 -0300

    first commit

---
+++ cards_project/deck.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+//Create a new tupe of 'deck'
+// wich is a slice of strings
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+
+	}
+
+	return cards
+
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
