d32dbfa99ef701b3904fa9083009dbf0c4baee00
Hugo Ferreira Ferro <hugoferreiraferro1@hotmail.com>
2026-09-24 17:21:47 -0300

    fedora

---
+++ channels/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c { //loop infinite
+		go func() {
+			time.Sleep(5 * time.Second)
+			checkLink(l, c)
+		}()
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- "Might be down i think"
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- link
+}
